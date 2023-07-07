c21e9f53bd176d617498cefa2e38c8e546aa30ac
amandabarr <amandabarryoga@gmail.com>
2023-07-07 15:12:40 -0700

    added additional functionality to allow user to add custom number of glasses of water

--- src/App.js
+++ src/App.js
@@ -3,65 +3,67 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 
-// export default function App() {
-//   return (
-//     <div>
-//       <h1>Don't Forget to Hydrate</h1>
-//       <WaterCounter />
-//     </div>
-//   );
-// }
-
-// function WaterCounter () {
-//   const [waterCount, setWaterCount] = useState(0);
-//   function handleClick() {
-//     setWaterCount(waterCount + 1);
-//   }
-//   return (
-//     <div>
-//     <h1>You've had {waterCount} glasses of water so far.</h1>
-//     <button className="water-count" onClick={handleClick}>Add Water</button>
-//     </div>
-//   );
-// }
-
 export default function App() {
   const [waterCount, setWaterCount] = useState(0);
+
   function handleClick() {
     setWaterCount(waterCount + 1);
   }
+
   function undoClick() {
     if (waterCount > 0) {
       setWaterCount(waterCount - 1);
     } else {
-      setWaterCount == 0;
+      setWaterCount(0);
     }
-
   }
+
+  function handleUserWaterCountChange() {
+    const userInput = parseInt(
+      document.getElementById("userWaterCount").value,
+      10
+    );
+    setWaterCount(waterCount + userInput);
+  }
+
   return (
     <div>
-      <h1>Don't Forget to Hydrate!</h1>
-      <WaterCounter waterCount={waterCount} onWaterCountClick={() => handleClick()} />
+      <h2>You've had {waterCount} glasses of water so far.</h2>
+      <input id="userWaterCount" type="number" />
+      <UserWaterCount onCustomSubmit={handleUserWaterCountChange} />
+      <WaterCounter
+        waterCount={waterCount}
+        onWaterCountClick={() => handleClick()}
+      />
       <WaterReducer onWaterReducerClick={() => undoClick()} />
     </div>
   );
 }
 
-function WaterCounter ({ waterCount, onWaterCountClick}) {
-
+function WaterCounter({ onWaterCountClick }) {
   return (
     <div>
-    <h2>You've had {waterCount} glasses of water so far.</h2>
-    <button className="WaterCount" onClick={onWaterCountClick}>Add 8oz Water</button>
+      <button className="waterCount" onClick={onWaterCountClick}>
+        Add 8oz Water
+      </button>
     </div>
   );
 }
 
-function WaterReducer ({ onWaterReducerClick }) {
-
+function WaterReducer({ onWaterReducerClick }) {
   return (
-  <div>
-    <button className="WaterReducer" onClick={onWaterReducerClick}>Oops! Remove 8oz Water</button>
-  </div>
+    <div>
+      <button className="waterReducer" onClick={onWaterReducerClick}>
+        Oops! Remove 8oz Water
+      </button>
+    </div>
+  );
+}
+
+function UserWaterCount({ onCustomSubmit }) {
+  return (
+    <div>
+      <button className="userWaterCount" onClick={onCustomSubmit}></button>
+    </div>
   );
 }
