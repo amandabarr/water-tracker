fbf3d411dea2361755a0abbe240a76fef57c576d
amandabarr <amandabarryoga@gmail.com>
2023-08-28 12:10:50 -0700

    added useEffect hooks to detect change in history, moving this functionality from the Submit and Remove Last Entry buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import React, { useState} from 'react';
+import React, { useEffect, useState} from 'react';
 import './App.css';
 
 export default function App() {
@@ -10,6 +10,20 @@ export default function App() {
   const waterTypes = ['Water', "Tea", 'Coffee', 'Juice'];
   const [selectedWaterType, setSelectedWaterType] = useState(waterTypes[0]);
 
+  useEffect( () => {
+    const storedHistory = JSON.parse(window.localStorage.getItem('history'));
+    if (storedHistory) {
+      setHistory(storedHistory);
+    }
+  }, []);
+
+  useEffect( () => {
+    if (history.length > 0) {
+      window.localStorage.setItem('history', JSON.stringify(history));
+    }
+  }, [history]);
+
+
   function handleUserWaterCountChange(event) {
     setUserInput(event.target.value);
   }
@@ -19,8 +33,9 @@ export default function App() {
     const userInputValue = parseInt(userInput, 10);
     if (!isNaN(userInputValue)) {
       const newUserWaterCount = waterCount + userInputValue;
+      const newHistoryEntry = {count: userInputValue, type: selectedWaterType };
       setWaterCount(newUserWaterCount);
-      setHistory([...history, {count: userInputValue, type: selectedWaterType }]);
+      setHistory([...history, newHistoryEntry]);
       setUserInput(""); // Clear the input field after submission
     }
   }
@@ -34,6 +49,13 @@ export default function App() {
       const lastInput = history[history.length - 1]["count"];
       setWaterCount(waterCount - lastInput);
       setHistory(history.slice(0, -1));
+    }
+  }
+
+  function clearHistory () {
+    if (history.length > 0) {
+      setHistory([]);
+      localStorage.clear();
     }
   }
 
@@ -56,6 +78,7 @@ export default function App() {
       />
       <WaterType waterTypes={waterTypes} selectedWaterType={selectedWaterType} onWaterTypeChange={handleWaterTypeChange} />
       <WaterReducer onWaterReducerClick={undoClick} />
+      <ClearHistory onClearHistoryClick={clearHistory} />
     </div>
   );
 }
@@ -93,6 +116,16 @@ function WaterType({ waterTypes, selectedWaterType, onWaterTypeChange}) {
           </option>
         ))}
       </select>
+    </div>
+  )
+}
+
+function ClearHistory({onClearHistoryClick}) {
+  return (
+    <div>
+      <button className="clearHistory" onClick={onClearHistoryClick}>
+        Clear History
+      </button>
     </div>
   )
 }
